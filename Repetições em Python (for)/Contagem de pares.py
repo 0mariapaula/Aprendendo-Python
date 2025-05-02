cc33a80f141b84c170c7a40756bc59b1091e8ee2
0mariapaula <0mariapaulaferreira@gmail.com>
2025-05-02 19:22:02 -0300

    estudos do dia 02 de Maio

---
+++ Repetições em Python (for)/Contagem de pares.py
@@ -0,0 +1,5 @@
+from time import sleep
+for n in range (0,51,2): #criar sequência de números
+    print(n) #guardar valores da contagem do range
+    #sleep(0.5)
+print('ACABOU!')
---
+++ Repetições em Python (for)/Contagem regressiva.py
@@ -0,0 +1,5 @@
+from time import sleep
+for cont in range (10,-1,-1): #criar sequência de números
+    print(cont) #guardar valores da contagem do range
+    sleep(0.5)
+print('ACABOU!')
