5b106592505ce9174b0762bd41feac2960698e7d
0mariapaula <0mariapaulaferreira@gmail.com>
2025-09-16 07:19:54 -0300

    Função de Contador (16/09)

---
+++ Funções em Python/Função de Contador.py
@@ -0,0 +1,22 @@
+def contador (i, f, p):
+   print (f'Contagem de {i} até {f} de {p} em {p}:')
+
+   if i < f:
+       cont = i
+       while cont <= f:
+           print (f'{cont}', end=' ', flush=True)
+           cont += p
+   else:
+       cont = i
+       while cont >= f:
+           print (f'{cont}', end=' ', flush=True)
+           cont -= p
+   print('FIM!')
+
+contador (1,10,1)
+contador (10,0,2)
+print('Agora é sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(ini, fim, passo)
